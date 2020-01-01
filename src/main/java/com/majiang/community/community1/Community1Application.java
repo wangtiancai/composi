9beffc9c6ea70cf89d014d4161daf341530e80da
86183 <874491161@qq.com>
2020-01-01 16:02:56 +0800

    1

---
+++ src/main/java/com/majiang/community/community1/Community1Application.java
@@ -0,0 +1,14 @@
+package com.majiang.community.community1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Community1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Community1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/majiang/community/community1/Community1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majiang.community.community1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Community1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
